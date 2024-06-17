5887e166c48c2b4cce23573ce551de0c7700a8ac
simone-kanzawa <simone.kanzawa@waterservicestech.com>
2024-06-17 13:27:55 -0300

    test: ensure SignUpController returns 400 if no name is provided

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
-@type {import('jest').Config} */
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
 const config = {
-  roots: ['<rootDir>/src']
-
-
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-
-  coverageProvider: "v8",
-
-  testEnvironment: "node",
-
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest',
   },
 };
 
